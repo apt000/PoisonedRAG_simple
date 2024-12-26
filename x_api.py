a80eb4df4774f6d1ec4a399c9bfc42dd0fd65311
apt000 <108866679+apt000@users.noreply.github.com>
2024-12-26 15:02:39 +0800

    Update x_api.py

--- x_api.py
+++ x_api.py
@@ -7,16 +7,6 @@ client = OpenAI(
     base_url="https://api.x.ai/v1",
 )
 
-'''completion = client.chat.completions.create(
-    model="grok-beta",
-    messages=[
-        {"role": "system", "content": "You are Grok, a chatbot inspired by the Hitchhikers Guide to the Galaxy."},
-        {"role": "user", "content": "What is the meaning of life, the universe, and everything?"},
-    ],
-)'''
-
-# print(completion.choices[0].message.content)
-
 def get_res(prompt):
     completion = client.chat.completions.create(
         model="grok-beta",
